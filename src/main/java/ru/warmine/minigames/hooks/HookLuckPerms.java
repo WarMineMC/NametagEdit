96315818639c61d4503eb129b284980e86be5ff1
jestkiytipok <theprosky@gmail.com>
2021-01-16 02:20:05 +0300

    fix: change LuckPerms API version from 5.0 to 4.4

--- src/main/java/ru/warmine/minigames/hooks/HookLuckPerms.java
+++ src/main/java/ru/warmine/minigames/hooks/HookLuckPerms.java
@@ -1,27 +1,26 @@
 package ru.warmine.minigames.hooks;
 
+import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.User;
+import me.lucko.luckperms.api.event.EventBus;
+import me.lucko.luckperms.api.event.user.UserDataRecalculateEvent;
 import ru.warmine.minigames.NametagHandler;
-import net.luckperms.api.LuckPerms;
-import net.luckperms.api.event.EventBus;
-import net.luckperms.api.event.user.UserDataRecalculateEvent;
-import net.luckperms.api.model.user.User;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 public class HookLuckPerms implements Listener {
-
-    private NametagHandler handler;
+    private final NametagHandler handler;
 
     public HookLuckPerms(NametagHandler handler) {
         this.handler = handler;
-        EventBus eventBus = Bukkit.getServicesManager().load(LuckPerms.class).getEventBus();
+        EventBus eventBus = Bukkit.getServicesManager().load(LuckPermsApi.class).getEventBus();
         eventBus.subscribe(handler.getPlugin(), UserDataRecalculateEvent.class, this::onUserDataRecalculateEvent);
     }
 
     private void onUserDataRecalculateEvent(UserDataRecalculateEvent event) {
         User user = event.getUser();
-        Player player = Bukkit.getPlayer(user.getUniqueId());
+        Player player = Bukkit.getPlayer(user.getUuid());
         if (player != null) {
             handler.applyTagToPlayer(player, false);
         }
